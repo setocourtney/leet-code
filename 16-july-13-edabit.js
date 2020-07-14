4a0d78bae4bde083dd6f6b2f09b71ea50425cf8f
setocourtney <courtneyjseto@gmail.com>
2020-07-13 17:06:19 -0700

    char at pos

--- 16-july-13-edabit.js
+++ 16-july-13-edabit.js
@@ -11,4 +11,23 @@
 
 // "C. B." ➞ true
 
-let x = /\s*([A-Z]\.){2,3}\s*;
+let x = /^ *([A-Z]\.)( [A-Z]\.){1,2} *$/;
+
+// Create a function that extracts the characters from an array (or a string) on odd or even positions, depending on the specifier. The string 'odd' for items on odd positions (1, 2, 3, ...) and 'even' for even positions (2, 4, 6, ...).
+
+// Examples
+// charAtPos([2, 4, 6, 8, 10], 'even') ➞ [4, 8]
+// // 4 & 8 occupy the 2nd & 4th positions
+
+// charAtPos('EDABIT', 'odd') ➞ 'EAI'
+// // 'E', 'A' and 'I' occupy the 1st, 3rd and 5th positions
+
+const charAtPos = (r, s) => {
+    let result = [];
+    for (let i = 0; i < r.length; i++) {
+        if (i % 2 === 0 && s === 'odd' || i % 2 !== 0 && s === 'even') {
+            result.push(r[i]);
+        }
+    }
+    return Array.isArray(r) ? result : result.join("");
+}
