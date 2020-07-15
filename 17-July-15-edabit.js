3d55833fd01beb683951c13429d9c4df72896e5c
setocourtney <courtneyjseto@gmail.com>
2020-07-15 15:56:18 -0700

    removed letter filter

--- 17-July-15-edabit.js
+++ 17-July-15-edabit.js
@@ -37,3 +37,13 @@ const countBoomerangs = (arr) => {
     }
     return count;
 }
+
+// Create a function that takes a variable number of groups of items, and returns the number of ways the items can be arranged, with one item from each group. Order does not matter.
+// Examples
+// combinations(2, 3) ➞ 6
+// combinations(3, 7, 4) ➞ 84
+// combinations(2, 3, 4, 5) ➞ 120
+
+const combinations = (...items) => {
+    return items.reduce((total, item) => item !== 0 ? total * item : total);
+}
