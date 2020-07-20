b04459650973e720cb05e470704e5d90fe5925d8
setocourtney <courtneyjseto@gmail.com>
2020-07-20 15:05:43 -0700

    common values

--- 20-july-20-edabit.js
+++ 20-july-20-edabit.js
@@ -21,6 +21,38 @@ const reverseOdd = (str) => {
     return arr.join(" ");
 }
 
-console.log( reverseOdd("Bananas") );
-console.log( reverseOdd("One two three four") );
-console.log( reverseOdd("Make sure uoy only esrever sdrow of ddo length") );
+// console.log( reverseOdd("Bananas") );
+// console.log( reverseOdd("One two three four") );
+// console.log( reverseOdd("Make sure uoy only esrever sdrow of ddo length") );
+
+// Create a function that takes two "sorted" arrays of numbers and returns an array of numbers which are common to both the input arrays.
+// commonElements([-1, 3, 4, 6, 7, 9], [1, 3]) ➞ [3]
+// commonElements([1, 3, 4, 6, 7, 9], [1, 2, 3, 4, 7, 10]) ➞ [1, 3, 4, 7]
+// commonElements([1, 2, 2, 2, 3, 4, 5], [1, 2, 4, 5]) ➞ [1, 2, 4, 5]
+// commonElements([1, 2, 3, 4, 5], [10, 12, 13, 15]) ➞ []
+
+const commonElements = (arr1, arr2) => {
+    let i = 0;
+    let j = 0;
+    let result = [];
+    while ( i < arr1.length && j < arr2.length ) {
+        if ( arr1[i] === arr2[j])  { 
+            if ( result.indexOf(arr1[i]) < 0 ) {
+                result.push(arr1[i]);
+            }
+            i++;
+            j++;
+        }
+        else if ( arr2[j] > arr1[i] ) {
+            i++;
+        } else {
+            j++
+        }
+    }
+    return result;
+}
+
+commonElements([-1, 3, 4, 6, 7, 9], [1, 3]);
+commonElements([1, 3, 4, 6, 7, 9], [1, 2, 3, 4, 7, 10]);
+commonElements([1, 2, 2, 2, 3, 4, 5], [1, 2, 4, 5]);
+commonElements([1, 2, 3, 4, 5], [10, 12, 13, 15]);
