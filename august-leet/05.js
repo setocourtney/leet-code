1050676a6df665d6e5ad850f70b49cb274915a9d
setocourtney <courtneyjseto@gmail.com>
2020-08-05 09:49:25 -0700

    05 - add word and search word - bst - incomplete:

---
+++ august-leet/05.js
@@ -0,0 +1,52 @@
+// Add and Search Word
+
+/*
+ Design a data structure that supports the following two operations:
+
+    void addWord(word)
+    bool search(word)
+    search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means it can represent any one letter.
+*/
+
+/* Tests:
+
+    addWord("bad")
+    addWord("dad")
+    addWord("mad")
+    search("pad") -> false
+    search("bad") -> true
+    search(".ad") -> true
+    search("b..") -> true
+*/
+
+/**
+ * Initialize your data structure here.
+ */
+var WordDictionary = function() {
+    
+};
+
+/**
+ * Adds a word into the data structure. 
+ * @param {string} word
+ * @return {void}
+ */
+WordDictionary.prototype.addWord = function(word) {
+    
+};
+
+/**
+ * Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. 
+ * @param {string} word
+ * @return {boolean}
+ */
+WordDictionary.prototype.search = function(word) {
+    
+};
+
+/** 
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = new WordDictionary()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
