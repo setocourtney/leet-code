839ceb3f2bc121bb621acec214ce74e3bb961d72
setocourtney <courtneyjseto@gmail.com>
2020-08-06 18:17:56 -0700

    06 - refractored

--- august-leet/06.js
+++ august-leet/06.js
@@ -21,16 +21,24 @@ var findDuplicates = function(nums) {
     }
     return Object.keys(count).filter(num => count[num] > 1);
 };
+/* ------------------------------------------------------------------------ */
+
+/*  Result: 33.3%
+    Runtime:
+        best case: O(n) - due to reverse()
+        worst case: O(2n)
+        average case: O(n)
+    Space: O(n)
+    Time to Complete: 25 min
+
+*/
 
 var findDuplicatesRefactored = function(nums) {
     let result = [];
-    if (nums.length === 2 && nums[0] === nums[1]) {
-        return [nums[0]];
-    };
     for (let i = 0; i < nums.length; i++) {
-        if (nums[Math.abs(nums[i])] >= 0) {
-            nums[Math.abs(nums[i])] = -nums[Math.abs(nums[i])];
-        } else if (Math.abs(nums[i]) !== nums.length) {
+        if (nums[Math.abs(nums[i]) - 1] > 0) {
+            nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1];
+        } else {
             result.push(Math.abs(nums[i]));
         }
     }
@@ -40,12 +48,12 @@ var findDuplicatesRefactored = function(nums) {
 console.log(findDuplicatesRefactored([4,3,2,7,8,2,3,1]));
 
 /* ------------------------------------------------------------------------ */
-
-/*  Result: 33.3%
+/*  Result: 82%
     Runtime:
         best case: O(n) - due to reverse()
         worst case: O(2n)
         average case: O(n)
-    Time to Complete: 25 min
+    Space: O(1)
+    Time to Complete: 60 min
 
 */
