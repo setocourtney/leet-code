aa50b3e810b5e0b7c8589f4cb3fe6d8e6471909a
setocourtney <courtneyjseto@gmail.com>
2020-08-16 18:37:34 -0700

    12 - finish pascals triangle

--- august-leet/12.js
+++ august-leet/12.js
@@ -8,6 +8,35 @@
 // Input: 3
 // Output: [1,3,3,1]
 
+// Entry k of row n = (n k) = n! / k!(n-k)!
+
 var getRow = function(rowIndex) {
-    
+    let result = [];
+    let nFact = getFactorial(rowIndex);
+    for (let i = 0; i < rowIndex + 1; i++) {
+        let val = nFact / (getFactorial(i) * getFactorial(rowIndex - i));
+        result.push(val);
+    }
+    return result;
 };
+
+let getFactorial = function(n) {
+    let result = 1;
+    for (let i = n; i >= 1; i--) {
+        result = result * i;
+    }
+    return result;
+}
+
+
+/* ------------------------------------------------------------------------ */
+
+/*  Result: 73.35%
+    Runtime:
+        best case: O(2n)
+        worst case: O(n^2)
+        average case: O(n^2)
+    Space: O(n)
+    Time to Complete: 45 min
+
+*/
